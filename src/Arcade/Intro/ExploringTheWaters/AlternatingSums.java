84d41fee205e7baa2dc7594004cc35830f05c965
Bozsum\Bozsumwv <bozsumwv@gmail.com>
2017-09-07 12:09:43 +0700

    No 14  :  alternatingSums

---
+++ src/Arcade/Intro/ExploringTheWaters/AlternatingSums.java
@@ -0,0 +1,18 @@
+package Arcade.Intro.ExploringTheWaters;
+
+public class AlternatingSums
+{
+    int[] alternatingSums(int[] a)
+    {
+        int[] sum = {0,0};
+        for (int i = 0; i < a.length; i++)
+        {
+            if(i % 2 == 0)
+                sum[0] += a[i];
+            else
+                sum[1] += a[i];
+        }
+        return sum;
+    }
+
+}

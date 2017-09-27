9c483a0768b3d3f8387837a533a87f36198e66f0
Bozsum\Bozsumwv <bozsumwv@gmail.com>
2017-09-27 21:44:35 +0700

    No 42  :  bishopAndPawn

---
+++ src/Arcade/Intro/DarkWilderness/BishopAndPawn.java
@@ -0,0 +1,9 @@
+package Arcade.Intro.DarkWilderness;
+
+public class BishopAndPawn
+{
+    boolean bishopAndPawn(String bishop, String pawn)
+    {
+        return Math.abs(bishop.charAt(0) - pawn.charAt(0))  == Math.abs(bishop.charAt(1) - pawn.charAt(1));
+    }
+}
